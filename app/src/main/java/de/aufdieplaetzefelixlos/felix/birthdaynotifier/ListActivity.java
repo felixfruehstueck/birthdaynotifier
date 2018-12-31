1e951d4fbc11a4ed653861a99a296625e8be5562
Felix <felix.m.kittler@gmail.com>
2018-12-31 18:40:21 +0100

    basic setup + app configuration

---
+++ app/src/main/java/de/aufdieplaetzefelixlos/felix/birthdaynotifier/ListActivity.java
@@ -0,0 +1,13 @@
+package de.aufdieplaetzefelixlos.felix.birthdaynotifier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
